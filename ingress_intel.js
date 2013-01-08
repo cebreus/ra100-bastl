49727e0670848ab1a1cc313ed576692eb70be257
ra100 <ra100x@gmail.com>
2013-01-08 15:54:58 +0100

    nacitavanie do textarea

--- ingress_intel.js
+++ ingress_intel.js
@@ -2,11 +2,12 @@
 // @name Ingress Intel enhance
 // @description Enhances Ingress Intel, maybe
 // @include http://www.ingress.com/intel
-// @include http://*.ingress.com/intel
+// @match http://www.ingress.com/intel
 // ==/UserScript==
 (function ()
 {
 	var players = [];
+	var time = 0; //unix epoch timestamp
 	
 	var headID = document.getElementsByTagName("head")[0];         
     var newScript = document.createElement('script');
@@ -18,14 +19,16 @@
     window.addEventListener('load', function (e)  {
     	var style = '<style>#enhance {z-index: 2000; position: absolute; left: 0px; top: 0px; display: block; background: #000; color: #59FBEA;}'+
     	'.button {cursor: pointer; background-color: #004F4A; color: #59FBEA; padding: 1px 15px; font-size: 13px; border: #59FBEA 1px solid; float: left;}'+
-    	'#portaltitle {padding: 0px 10px; float: left; border: 1px solid #59FBEA; width: 427px; height: 23px; font-size: 12px;}'
-    	+'#log {clear: both; float: left; width: auto; max-height: 400px; overflow: auto; background: #000; color: #59FBEA; border: #59FBEA 1px solid; z-index: 20; display: block; font-size: 11px;}'+
+    	'#portaltitle {padding: 0px 10px; float: left; border: 1px solid #59FBEA; width: 200px; height: 23px; font-size: 12px;}'
+    	+'#log {clear: both; float: left; width: 770px; max-height: 500px; overflow: auto; background: #000; color: #59FBEA; border: #59FBEA 1px solid; z-index: 20; display: block; font-size: 11px; min-height: 12px;}'+
     	'.row {clear: both; float: left;}'+
     	'.team {clear: both; float: left; font-weight: bold; font-size: 12px;}'+
     	'.name {font-weight: bold; padding-right: 5px;}'+
     	'.level {font-color: red; padding-right: 10px;}'+
     	'.areas {padding-left: 5px;}'+
-    	'.area {border-left: 1px solid #59FBEA; padding-left: 5px; padding-right: 5px;}'    	
+    	'.area {padding-left: 5px; padding-right: 5px;}' +
+    	'.message {clear: both; float: left; font-size: 12px; width: auto; margin-left: 5px; margin-right: 5px;}'+
+    	'#textarea {display: none; position: absolute; z-index: 2000;}'   	
     	+'</style>';
 		var tag = '<div id="enhance"></div>';
 		$("head").append(style);
@@ -33,26 +36,38 @@
 		$("#nav").empty();
 		$("#enhance").append('<div class="button" id="copytitle">copy portal name</div>');
 		$("#copytitle").click(function() {
-			$("#portaltitle").text($("#portal_primary_title").text());
+			$("#portaltitle").val($("#portal_primary_title").text());
 		});
-		$("#enhance").append('<div class="text" id="portaltitle"></div>');
+		$("#enhance").append('<input class="text" id="portaltitle" type="text"></input>');
 
 		$("#enhance").append('<div class="button" id="loaddata">Get portals</div>');
 		$("#loaddata").click(function() {
 			ing_ajaxCall();
 		});
+		
+		
 		$("#enhance").append('<div class="button" id="showlog">Toggle log</div>');
 		$("#showlog").click(function() {
 			$("#log").toggle();
 		});
 		
+		$("body").append('<textarea rows="20" cols="60" id="textarea"></textarea>');
+		$("#enhance").append('<div class="button" id="showtextarea">Toggle textarea</div>');
+		$("#showtextarea").click(function() {
+			$("#textarea").toggle();
+		});	
+		
+		$("body").append('<textarea rows="20" cols="60" id="textarea"></textarea>');
+		
 		$("#enhance").append('<div id="log"></div>');
+		$("#log").show();
 
     }, false);
 
 	
 
 	function ing_ajaxCall() {
+		ing_addLog("loading portals...");
 		var requestdata = {
 			"minLevelOfDetail":-1,
 			"boundsParamsList":	[{"id":"0120212302","minLatE6":49837982,"minLngE6":14062500,"maxLatE6":50289339,"maxLngE6":14765625,"qk":"0120212302"}],
@@ -78,6 +93,7 @@
 	}
 	
 	function ing_ajaxGetNames(guids) {
+		ing_addLog("getting codenames...");
 		var requestdata = {"guids":	guids,
 				"method":"dashboard.getPlayersByGuids"};
 
@@ -94,7 +110,6 @@
 				console.log("error");
 			},
 			complete: function(a) {
-				//console.log(a);
 			}
 		});	
 	}
@@ -102,16 +117,20 @@
 	function ing_processData(data) {
 		var portals = [];
 		var count = 0;
-		
+		var processed = 0;
+
 		$.each(data.result.map, function(i, dat) {
 			$.each(dat.gameEntities, function(j, data) {
-				portals[data[0]] = [];
-				portals[data[0]]["team"] = "NEUTRAL";
-				portals[data[0]]["address"] = "";
-				portals[data[0]]["resonators"] = "";
-				if (data[2].resonatorArray) {portals[data[0]]["resonators"] = data[2].resonatorArray.resonators};
-				portals[data[0]]["team"] = data[2].controllingTeam.team;
-				if (data[2].portalV2) {portals[data[0]]["address"] = ing_findPraguePart(data[2].portalV2.descriptiveText.ADDRESS);} 
+				if (data[1] > 1357581222385) {
+					portals[data[0]] = [];
+					portals[data[0]]["team"] = "NEUTRAL";
+					portals[data[0]]["address"] = "";
+					portals[data[0]]["resonators"] = "";
+					if (data[2].resonatorArray) {portals[data[0]]["resonators"] = data[2].resonatorArray.resonators};
+					portals[data[0]]["team"] = data[2].controllingTeam.team;
+					if (data[2].portalV2) {portals[data[0]]["address"] = ing_findPraguePart(data[2].portalV2.descriptiveText.ADDRESS);} 
+					processed = processed +1;
+				};
 				count = count +1;
 			});
 		});
@@ -134,49 +153,49 @@
 			guids.push(key);
 		}
 		
+		ing_addLog(processed+' portals processed from '+count+' total.');
+		
 		ing_ajaxGetNames(guids);
+	}
+	
+	function ing_addLog(str) {
+		$("#log").append('<span class="message">'+str+'</span>');
 	}
 	
 	function ing_processUsers(data) {
 		$.each(data.result, function(i, users) {
 			players[users.guid]["name"] = users.nickname;
 		});
-		
+		ing_addLog("codenames loaded");
 		ing_showPlayers();
+		
 	}
 	
 	function ing_showPlayers() {
-		$("#log").append('<span class="team resistance">RESISTANCE</span>');
-		for (i in players) {
-			var player = players[i];
-			if (player.team == "RESISTANCE") {
-				var	areas = "";
-				for (j in player["areas"]) {
-					areas = areas+'<span class="area">'+j+" L"+player["areas"][j]+'</span>;';
-				}
-				$("#log").append('<div class="row resistance"><span class="name">'+player["name"]+'</span>;'+
-						'<span class="level">'+player["level"]+'</span>;'+
-						'<span class="areas">'+areas+'</span>'+
-						'</row>');
-			}
-		}
+		$("#textarea").val($("#textarea").val()+'ENLIGHTENED\n');
 		
-		$("#log").append('<span class="team enlightened">ENLIGHTENED</span>');
 		for (i in players) {
 			var player = players[i];
 			if (player.team == "ALIENS") {
 				var	areas = "";
 				for (j in player["areas"]) {
-					areas = areas+'<span class="area">'+j+" L"+player["areas"][j]+'</span>;';
+					areas = areas+j+" L"+player["areas"][j]+',';
 				}
-				$("#log").append('<div class="row enlightened"><span class="name">'+player["name"]+'</span>;'+
-						'<span class="level">'+player["level"]+'</span>;'+
-						'<span class="areas">'+areas+'</span>'+
-						'</row>');
+				$("#textarea").val($("#textarea").val()+player["name"]+','+player["level"]+','+areas+'\n');
 			}
 		}
-		$("#log").show();
-		console.log("done");
+		$("#textarea").val($("#textarea").val()+'\nRESISTANCE\n');
+		for (i in players) {
+			var player = players[i];
+			if (player.team == "RESISTANCE") {
+				var	areas = "";
+				for (j in player["areas"]) {
+					areas = areas+j+" L"+player["areas"][j]+',';
+				}
+				$("#textarea").val($("#textarea").val()+player["name"]+','+player["level"]+','+areas+'\n');
+			}
+		}
+		$("#textarea").show();
 	}
  
  
